8afe713acd1dd1770ea7fe24840332f8222a5c61
UrMoms <louisvannoorden@gmail.com>
2019-01-14 23:12:03 +0100

    clean up

--- index.js
+++ index.js
@@ -2,16 +2,11 @@
 const record = require('node-record-lpcm16');
 const Detector = require('snowboy').Detector;
 const Models = require('snowboy').Models;
-const fs = require('fs');
-const wav = require('wav');
-const FileWriter = require('wav').FileWriter;
-const portAudio = require('node-portaudio');
 const googleSpeech = require('@google-cloud/speech')
 const speechResolveService = require('./service/speechResolve')
-const { fromEvent, combineLatest,timer,zip,merge ,of} = require('rxjs');
-const { map,take,switchMap,mapTo,race, filter ,tap,throttleTime,exhaustMap, takeUntil, windowCount, bufferToggle, bufferCount,delay} = require('rxjs/operators');
-//speechResolveService.resolveCommandLine("play track 1");
-//var speech = new googleSpeech.SpeechClient();
+const { fromEvent,merge ,of} = require('rxjs');
+const { map,take,switchMap,mapTo,filter ,tap,throttleTime, takeUntil, bufferToggle, bufferCount,delay} = require('rxjs/operators');
+let speech = new googleSpeech.SpeechClient();
 const Mopidy = require ('mopidy');
 let mopidy = new Mopidy({
     webSocketUrl: "ws://localhost:6680/mopidy/ws/",
@@ -26,12 +21,12 @@ const config = {
   languageCode: 'en-US'
 }
 
-/*function sendToSpeechApi(audioBytes){
+function sendToSpeechApi(audioBytes){
   let request = {
     audio:{content: audioBytes},
     config:config
   }
-  console.log(request)
+
   speech.recognize(request)
     .then(data => {
       const response = data[0];
@@ -39,39 +34,12 @@ const config = {
       console.log(cmdLine);
       speechResolveService.resolveCommandLine(cmdLine,mopidy);
   })
-}*/
+}
 
 mopidy.on("state:online", function () {
   startMicListening();
   console.log("Mic listening")
 }); 
-
-
-// the "format" event gets emitted at the end of the WAVE header
-const wavReader = new wav.Reader();
-/*appFeedbackAudioOut("resources/ding.wav")
-function appFeedbackAudioOut(filePath){
-    const readStream = fs.createReadStream(filePath); 
-    
-
-    wavReader.on('format', function (format) {
-        Object.keys(format).forEach(k=> console.log(k + ":" + format[k]))
-    
-        var ao = new portAudio.AudioOutput({
-            channelCount: format.channels,
-            sampleFormat: format.bitDepth,
-            sampleRate: format.sampleRate,
-            deviceId : -1 // Use -1 or omit the deviceId to select the default device
-           });
-        
-      // the WAVE header is stripped from the output of the reader
-        wavReader.pipe(ao); 
-        ao.start(); 
-        readStream.on('end', () => ao.end());
-    });
-
-    readStream.pipe(wavReader);  
-}*/
 
 models.add({
   file: 'resources/models/snowboy.umdl',
@@ -86,16 +54,13 @@ const detector = new Detector({
   applyFrontend: true
 });
 
-/*detector.on('hotword', function (index, hotword, buffer) {
-    // <buffer> contains the last chunk of the audio that triggers the "hotword"
-    // event. It could be written to a wav stream. You will have to use it
-    // together with the <buffer> in the "sound" event if you want to get audio
-    // data after the hotword.
-    console.log("you got snowboyed")
-    mopidy.playback.pause();
-    bufferArr.push(buffer);
-    hotwordDetected=true;
-  });*/
+function startMicListening() {
+  const mic = record.start({
+                      threshold: 0,
+                      verbose: false
+                    });    
+  mic.pipe(detector);
+}
 
 //snowboy event streams
 const silenceDetected = fromEvent(detector, 'silence');
@@ -124,7 +89,7 @@ const utterance = allSounds.pipe(
 );
 
 //send to google
-utterance.subscribe(res => console.log(res));
+utterance.subscribe(audio => sendToSpeechApi(audio));
 
 //mopidy event streams
 const playBackStateChanged = fromEvent(mopidy, 'event:playbackStateChanged').pipe(
@@ -135,16 +100,5 @@ const playBackStoppedOrPaused = playBackStateChanged
         filter(state => state.new_state === "stopped" || state.new_state === "paused"),
     );
 
-detector.on('error', function () {
-  console.log('error');
-});
 
-function startMicListening() {
-    const mic = record.start({
-        threshold: 0,
-        verbose: false
-      });
-      
-    mic.pipe(detector);
-}
 
